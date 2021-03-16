de9540597fcf97473085800f2cc7c6eacabe68b0
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2021-03-16 13:59:53 -0300

    Melhorado códigos

--- js/index.js
+++ js/index.js
@@ -1,44 +1,46 @@
-if (window.location.hostname != 'localhost' && window.location.hostname != '127.0.0.1' && window.location.protocol == 'http:') {
+if (window.location.hostname !== 'localhost' &&
+	window.location.hostname !== '127.0.0.1' &&
+	window.location.protocol === 'http:') {
 	window.location.protocol = 'https:'
 }
 
-var recording, paused = false, timer, defaultTitle = document.title
-var voutputEl = document.getElementById('voutput')
-var vwidthEl = document.getElementById('vwidth')
-var vheightEl = document.getElementById('vheight')
-var vframerateEl = document.getElementById('vframerate')
-var vdelayEl = document.getElementById('vdelay')
-var vstartEl = document.getElementById('vstart')
-var vpauseEl = document.getElementById('vpause')
-var vstopEl = document.getElementById('vstop')
-var vdownloadEl = document.getElementById('vdownload')
-var vtimerEl = document.getElementById('vtimer')
+var recording, stream, paused = false, timer, defaultTitle = document.title
+const $output = document.getElementById('voutput')
+const $width = document.getElementById('vwidth')
+const $height = document.getElementById('vheight')
+const $framerate = document.getElementById('vframerate')
+const $delay = document.getElementById('vdelay')
+const $start = document.getElementById('vstart')
+const $pause = document.getElementById('vpause')
+const $stop = document.getElementById('vstop')
+const $download = document.getElementById('vdownload')
+const $timer = document.getElementById('vtimer')
 
-voutputEl.style.display = 'none'
-vpauseEl.style.display = 'none'
-vstopEl.style.display = 'none'
-vdownloadEl.style.display = 'none'
+$output.style.display = 'none'
+$pause.style.display = 'none'
+$stop.style.display = 'none'
+$download.style.display = 'none'
 
-vstartEl.onclick = startCapturing
-vpauseEl.onclick = pauseCapturing
-vstopEl.onclick = stopCapturing
-vdownloadEl.onclick = downloadRecording
+$start.onclick = startCapturing
+$pause.onclick = pauseCapturing
+$stop.onclick = stopCapturing
+$download.onclick = downloadRecording
 
 if (!(navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia)) {
-	vtimerEl.innerHTML = 'Your browser does not support screen capture!'
-	vwidthEl.disabled = true
-	vheightEl.disabled = true
-	vframerateEl.disabled = true
-	vdelayEl.disabled = true
-	vstartEl.disabled = true
+	$timer.innerHTML = 'Your browser does not support screen capture!'
+	$width.disabled = true
+	$height.disabled = true
+	$framerate.disabled = true
+	$delay.disabled = true
+	$start.disabled = true
 }
 
 function startScreenCapture() {
 	return navigator.mediaDevices.getDisplayMedia({
 		video: {
-			width: vwidthEl.value,
-			height: vheightEl.value,
-			frameRate: vframerateEl.value
+			width: $width.value,
+			height: $height.value,
+			frameRate: $framerate.value
 		},
 		audio: true
 	})
@@ -47,14 +49,15 @@ function startScreenCapture() {
 async function startCapturing() {
 	window.URL.revokeObjectURL(recording)
 	chunks = []
-	stream = await startScreenCapture()
-	vstartEl.style.display = 'none'
-	vpauseEl.disabled = true
-	vstopEl.disabled = true
-	voutputEl.style.display = 'none'
-	vpauseEl.style.display = 'inline-block'
-	vstopEl.style.display = 'inline-block'
-	vdownloadEl.style.display = 'none'
+	stream = await startScreenCapture().catch(() => { })
+	if (!stream) return
+	$start.style.display = 'none'
+	$pause.disabled = true
+	$stop.disabled = true
+	$output.style.display = 'none'
+	$pause.style.display = 'inline-block'
+	$stop.style.display = 'inline-block'
+	$download.style.display = 'none'
 
 	stream.addEventListener('inactive', stopCapturing)
 	mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' })
@@ -64,43 +67,43 @@ async function startCapturing() {
 		}
 	})
 	timer = new Stopwatch(null, 1000, false)
-	timer.setElapsed(null, null, parseInt(vdelayEl.value))
+	timer.setElapsed(null, null, parseInt($delay.value))
 	timer.setListener(timer => {
-		vtimerEl.innerHTML = 'Starting at: ' + timer.toString()
+		$timer.innerHTML = 'Starting at: ' + timer.toString()
 		document.title = `(-${timer.toString()}) ${defaultTitle}`
 	})
 	timer.start()
-	vtimerEl.innerHTML = 'Starting at: ' + timer.toString()
+	$timer.innerHTML = 'Starting at: ' + timer.toString()
 	document.title = `(-${timer.toString()}) ${defaultTitle}`
 
 	setTimeout(() => {
 		timer.stop()
 		mediaRecorder.start(10)
-		vpauseEl.disabled = false
-		vstopEl.disabled = false
+		$pause.disabled = false
+		$stop.disabled = false
 
 		timer = new Stopwatch(null, 1000)
 		timer.setListener(timer => {
-			vtimerEl.innerHTML = timer.toString()
+			$timer.innerHTML = timer.toString()
 			document.title = `(${timer.toString()}) ${defaultTitle}`
 		})
 		timer.start()
-		vtimerEl.innerHTML = timer.toString()
+		$timer.innerHTML = timer.toString()
 		document.title = `(${timer.toString()}) ${defaultTitle}`
-	}, vdelayEl.value * 1000)
+	}, $delay.value * 1000)
 }
 
 function pauseCapturing() {
 	if (paused) {
-		vpauseEl.children[0].classList.remove('mdi-play')
-		vpauseEl.children[0].classList.add('mdi-pause')
+		$pause.children[0].classList.remove('mdi-play')
+		$pause.children[0].classList.add('mdi-pause')
 		document.title = `(${timer.toString()}) ${defaultTitle}`
 		mediaRecorder.resume()
 		timer.start()
 		paused = false
 	} else {
-		vpauseEl.children[0].classList.remove('mdi-pause')
-		vpauseEl.children[0].classList.add('mdi-play')
+		$pause.children[0].classList.remove('mdi-pause')
+		$pause.children[0].classList.add('mdi-play')
 		document.title = `(⏸ ${timer.toString()}) ${defaultTitle}`
 		mediaRecorder.pause()
 		timer.stop()
@@ -109,32 +112,37 @@ function pauseCapturing() {
 }
 
 function stopCapturing() {
-	vstartEl.style.display = 'inline-block'
-	vpauseEl.style.display = 'none'
-	vstopEl.style.display = 'none'
-	vdownloadEl.style.display = 'inline-block'
-	voutputEl.style.display = 'block'
+	$start.style.display = 'inline-block'
+	$pause.style.display = 'none'
+	$stop.style.display = 'none'
+	$download.style.display = 'inline-block'
+	$output.style.display = 'block'
 	timer.stop()
 	document.title = defaultTitle
-	vpauseEl.children[0].classList.remove('mdi-play')
-	vpauseEl.children[0].classList.add('mdi-pause')
+	$pause.children[0].classList.remove('mdi-play')
+	$pause.children[0].classList.add('mdi-pause')
 	paused = false
 
 	stream.removeEventListener('inactive', stopCapturing)
 	mediaRecorder.stop()
 	stream.getTracks().forEach(track => track.stop())
+	stream = undefined
 	const blob = new Blob(chunks, { type: 'video/webm' })
 	ysFixWebmDuration(blob, timer.totalElapsed, blob => {
 		recording = window.URL.createObjectURL(blob)
-		voutputEl.src = recording
+		$output.src = recording
 	})
 }
 
 function downloadRecording() {
 	const downloadLink = document.createElement('a')
 	downloadLink.href = recording
-	downloadLink.download = 'screen-recording.webm'
+	downloadLink.download = `screen-recording-${Date.now()}.webm`
 	downloadLink.style.display = 'none'
 	document.body.appendChild(downloadLink)
 	downloadLink.click()
+}
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js')
 }
